1fb07105ec9a696c43cbe5e19a0a288aee7a9f50
krishjan05 <krishjan05@gmail.com>
2016-10-12 23:17:46 -0400

    Implemented the logic and design for calculating the kinetic energy

---
+++ Assignment6Group1/Assignment6Group1/VM.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6Group1
+{
+    class VM : INotifyPropertyChanged
+    {
+        public double Mass
+        {
+            get { return _mass; }
+            set { _mass = value; OnPropertyChanged(); }
+        }
+        private double _mass;
+        public double Velocity
+        {
+            get { return _velocity; }
+            set { _velocity = value; OnPropertyChanged(); }
+        }private double _velocity;
+
+        public double KE
+        {
+            get { return _KE; }
+            set { _KE = value; OnPropertyChanged(); }
+        }private double _KE;
+
+        public double CalcKineticEnergy(int mass, int velocity)
+        {
+            double KE = (0.5 * mass) * (velocity * velocity);
+            return KE;
+        }
+        #region
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string caller = "")
+        {
+            // make sure only to call this if the value actually changes
+
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(caller));
+            }
+        }
+        #endregion
+    }
+}
